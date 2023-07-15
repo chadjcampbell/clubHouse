d38a78739773b70d8d4011c59b6c6f5b1dbab2e2
chadjcampbell <chadjcampbell@gmail.com>
2023-07-15 16:42:48 -0400

    start on front-end, add deps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,3 @@
-import "./App.css";
-
-function App() {
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
