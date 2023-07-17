c3098f351f4ef6dbf29c36f2c4090ff834a843d1
chadjcampbell <chadjcampbell@gmail.com>
2023-07-17 17:25:22 -0400

    build front end registration form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -65,6 +65,7 @@ const Login = () => {
                   name="email"
                   id="email"
                   placeholder="Email address"
+                  value={formData.email}
                 />
                 <label
                   htmlFor="email"
@@ -81,6 +82,7 @@ const Login = () => {
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   name="password"
                   id="password"
+                  placeholder="Password"
                   value={formData.password}
                 />
                 <label
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,5 +1,147 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Input, Ripple, initTE } from "tw-elements";
+import LoadingSpinner from "../components/LoadingSpinner";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Register = () => {
-  return <h2>Register</h2>;
+  initTE({ Input, Ripple });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value;
+    setFormData({ ...formData, [event.target.name]: value });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3000/api/users/login", {
+        email: formData.email,
+        password: formData.password,
+      });
+      navigate("/dashboard");
+      toast.success("Successfully logged in");
+    } catch {
+      toast.error("Invalid username or password");
+      setFormData(initialState);
+      setLoading(false);
+    }
+  };
+
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
+    <section className="h-screen">
+      <div className="container h-full px-6 py-24">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div className=" mb-12 md:mb-0 md:w-8/12 lg:w-1/3">
+            <Link className="flex flex-col items-center justify-center" to="/">
+              <img
+                src="/treehouse.webp"
+                className="w-full"
+                alt="Clubhouse logo"
+              />
+              <h2 className="text-primary">Back to Clubhouse</h2>
+            </Link>
+          </div>
+          <div className="mb-3 md:w-8/12 lg:ml-6 lg:w-5/12">
+            <form autoComplete="off" onSubmit={handleSubmit}>
+              <h1 className="mb-5 text-2xl">Join the Club!</h1>
+
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  onChange={handleInputChange}
+                  type="email"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  name="email"
+                  id="email"
+                  placeholder="Email address"
+                  value={formData.email}
+                />
+                <label
+                  htmlFor="email"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Email address
+                </label>
+              </div>
+
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  onChange={handleInputChange}
+                  type="password"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  name="password"
+                  id="password"
+                  value={formData.password}
+                />
+                <label
+                  htmlFor="password"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  onChange={handleInputChange}
+                  type="password"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  value={formData.confirmPassword}
+                />
+                <label
+                  htmlFor="confirmPassword"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  onChange={handleInputChange}
+                  autoComplete="false"
+                  type="username"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                  value={formData.username}
+                />
+                <label
+                  htmlFor="username"
+                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                >
+                  Username
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
